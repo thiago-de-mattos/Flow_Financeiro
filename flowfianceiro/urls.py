b45aa04f9bb9d73e1170c8c9c8e9979fab07b835
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-06-08 23:45:23 -0300

    feat: configura rotas do app core e do projeto

--- flowfianceiro/urls.py
+++ flowfianceiro/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    path('inicio', include('core.urls')), 
+    path("admin/", admin.site.urls),
+    path("",       include("core.urls")),
 ]
