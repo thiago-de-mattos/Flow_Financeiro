c9b97daf3e9992e837423ca63c360a67d63a053a
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-06-17 01:05:06 -0300

    fix: corrige nome da pasta

--- flowfinanceiro/settings.py
+++ flowfinanceiro/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'flowfianceiro.urls'
+ROOT_URLCONF = 'flowfinanceiro.urls'
 
 TEMPLATES = [
     {
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'flowfianceiro.wsgi.application'
+WSGI_APPLICATION = 'flowfinanceiro.wsgi.application'
 
 
 # Database
--- flowfinanceiro/wsgi.py
+++ flowfinanceiro/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowfianceiro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flowfinanceiro.settings')
 
 application = get_wsgi_application()
